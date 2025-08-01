9b9a783364d881950fff933fd5c81b662d28aa33
Barry Sanoussa <barrysanoussa19@gmail.com>
2025-08-01 09:28:27 +0100

    big O file creation

---
+++ bigO.py
@@ -0,0 +1,19 @@
+# WHAT IS BIG O NOTATION?  
+
+# Big O notation is a mathematical representation used to describe the performance or complexity of an algorithm . 
+# It specifically describes the worst-case scenario in terms of time or space complexity as the input size grows . 
+#  C'est quoi le BIG O 
+
+# Le big O nous permet de mesurer la performance ou la complexité de notre algo en fonction de la variation de la taille de nos données . 
+# C'est une represenation mathematique qui decrit comment est-ce que le temps evolue quand  la taille de nos donnees augmente 
+
+# Big O notation est  une representation mathematique utilisé pour decrire la performance ou la complixité d'un algorithme . 
+
+
+
+#  Big  
+
+
+
+
+
